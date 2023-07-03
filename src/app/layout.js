36b73ee8e0a4b9b905e9a9f08c78b0907b6f5516
Aman Raj Rana <mail2amanrana@gmail.com>
2023-07-03 13:39:19 +0530

    Refactor: Update site desctiption

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Markham College of Commerce, Hazaribagh",
-  description: "Markham College of commerce",
+  description:
+    "Welcome to Markham College of Commerce, located in Hazaribagh, Jharkhand. We offer a wide range of UG, PG, and Vocational programs such as BCA, BBA, BJMC, and BMLT. As a government-owned college affiliated with Vinoba Bhave University, we provide quality education for 12th pass students seeking UG degrees and specialized vocational programs. Explore our comprehensive curriculum and dynamic learning environment to shape a successful future.",
+
   viewport: {
     width: "device-width",
     initialScale: 1,
