dd3024fad93eb5630ccc6c14008f6817aa9269cd
Aman Raj Rana <mail2amanrana@gmail.com>
2023-07-21 16:40:43 +0530

    Add: Animation in hero section

--- src/components/public/HomeComponents/hero/Index.jsx
+++ src/components/public/HomeComponents/hero/Index.jsx
@@ -9,15 +9,24 @@ const Hero = () => {
       <div className="flex flex-col gap-4 justify-center h-full max-w-screen-xl mx-auto text-white">
         {/* ========== Section Text START ========== */}
         <h2 className="flex flex-col text-4xl md:text-5xl lg:text-6xl leading-[3rem] md:leading-[4rem] lg:leading-[5rem] font-bold ">
-          <span className="tracking-wider">Unlock Your</span>
-          <span className="tracking-wider">Education Dreams</span>
-          <span className="tracking-wider">With Us!!!</span>
+          <span className={`${styles.slideRightAnimation} tracking-wider`}>
+            Unlock Your
+          </span>
+
+          <span className={`${styles.slideRightAnimation} tracking-wider`}>
+            Education Dreams
+          </span>
+
+          <span className={`${styles.slideRightAnimation} tracking-wider`}>
+            With Us!!!
+          </span>
         </h2>
-        <p className="leading-8 my-4 max-w-sm">
+        <p className={`${styles.slideUpAnimation} leading-8 my-4 max-w-sm`}>
           Empowering students with education for a brighter future. Welcome to
           our college
         </p>
-        <div className="my-8">
+
+        <div className={`${styles.slideUpAnimation} my-8`}>
           <Button text={"EXPLORE &rarr;"} href={"#"} />
         </div>
         {/* ========== Section Text END ========== */}
