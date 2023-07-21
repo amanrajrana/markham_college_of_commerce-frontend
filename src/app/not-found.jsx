87b414f9d51a43338d2b990b8e4aabf02d7abcf4
Aman Raj Rana <mail2amanrana@gmail.com>
2023-07-21 14:10:33 +0530

    Add: 404 page in app/

---
+++ src/app/not-found.jsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+const StudentDashboard = () => {
+  return (
+    <div className="text-center bg-black text-white w-full h-screen flex flex-col gap-y-8 justify-center items-center text-2xl tracking-widest">
+      <div className="text-7xl font-bold tracking-widest">404</div>
+      <div>
+        We are under development!
+        <br />
+        come back soon :{")"}
+      </div>
+    </div>
+  );
+};
+
+export default StudentDashboard;
