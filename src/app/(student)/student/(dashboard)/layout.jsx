92f063671153f9ac62e44aefb96d6f85d9f82640
Aman Raj Rana <mail2amanrana@gmail.com>
2023-08-18 16:38:17 +0530

    Remove MetaData

--- src/app/(student)/student/(dashboard)/layout.jsx
+++ src/app/(student)/student/(dashboard)/layout.jsx
@@ -9,12 +9,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
-export const metadata = {
-  title: "Student Dashboard || Markham College of Commerce, Hazaribagh",
-  description:
-    "Welcome to Markham College of Commerce, located in Hazaribagh, Jharkhand. We offer a wide range of UG, PG, and Vocational programs such as BCA, BBA, BJMC, and BMLT. As a government-owned college affiliated with Vinoba Bhave University, we provide quality education for 12th pass students seeking UG degrees and specialized vocational programs. Explore our comprehensive curriculum and dynamic learning environment to shape a successful future.",
-};
-
 export default function RootLayout({ children }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
